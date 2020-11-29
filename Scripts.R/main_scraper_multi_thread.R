9318314f4a4043f1175fc4efd2b9715bcfb40f29
quant-manager <75098080+quant-manager@users.noreply.github.com>
2020-11-28 16:21:32 -0800

    Fixed timeout bug.

--- Scripts.R/main_scraper_multi_thread.R
+++ Scripts.R/main_scraper_multi_thread.R
@@ -14,9 +14,9 @@ library("doParallel")
 sa          <- c("AK","AR","CO","DE","FL","GA","HI","KY","LA","MI","MN","MT","NE","NM","NC","ND","OK","PA","SC","SD","WA","WV")
 pc          <- c(   1,   1,   1,   1,   2,   1,   1,   1,   1,   2,   1,   1,   1,   1,   1,   1,   1,   2,   1,   1,   1,   1)
 pv          <- c("General-", "GeneralConcatenator-")
-i           <- 6 # for all i | 1 <= i <= length(sa)
-sTimeStartG <- "2020-11-03T23:40:53.084Z" # set around 11/03/2020
-sTimeEndG   <- "2020-11-03T23:40:54.083Z"
+i           <- 2 # for all i | 1 <= i <= length(sa)
+sTimeStartG <- "2020-11-03T23:59:59.999Z" # set around 11/03/2020
+sTimeEndG   <- "2020-11-04T00:00:30.000Z"
 dTimeStartG <- as.numeric(strptime(sTimeStartG,"%Y-%m-%dT%H:%M:%OSZ"))*1000.0 + 0.001
 dTimeEndG   <- as.numeric(strptime(sTimeEndG,  "%Y-%m-%dT%H:%M:%OSZ"))*1000.0 + 0.001
 UseCores    <- parallel::detectCores()-1
@@ -39,7 +39,11 @@ res_global <- foreach(j=1:length(intervals[[1]])) %dopar%
       # ts <- "latest"
       json_url <- paste0("https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/precincts/",
                          sa[i],pv[pc[i]],ts,".json")
-      if(httr::status_code(httr::GET(json_url, timeout(20))) == 200)
+      status_code <- tryCatch({httr::status_code(httr::GET(json_url, config = httr::config(connecttimeout = 20)))},
+                              error=function(cond){return(200)},
+                              warning=function(cond){return(200)})#,
+                              #finally={})
+      if(status_code == 200)
       {
         res_local <- paste0(res_local,"|",json_url)
       }
