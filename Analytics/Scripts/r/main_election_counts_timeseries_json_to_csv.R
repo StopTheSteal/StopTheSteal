07442fbb51e7a51d1ccf07a30ccc75385d71e68c
quant-manager <75098080+quant-manager@users.noreply.github.com>
2021-01-09 15:33:52 -0800

    Added basic faunctionality for creating plots.

--- Analytics/Scripts/r/main_election_counts_timeseries_json_to_csv.R
+++ Analytics/Scripts/r/main_election_counts_timeseries_json_to_csv.R
@@ -4,15 +4,18 @@ gc()
 options(digits=22)
 options(digits.secs=3)
 
-list.of.packages <- c("jsonlite", "httr", "caroline")
+list.of.packages <- c("jsonlite", "httr", "caroline", "dplyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 library("jsonlite")
 library("httr")
 library("caroline")
+library("dplyr")
 
 base_dir <- getwd()
+# "GenElec2020/VoterRollsAnalysis"
+# "GenElec2020/StopTheSteal/Analytics/Scripts/r"
 if(!endsWith(base_dir,"StopTheSteal/Analytics/Scripts/r"))
 {
   stop(paste0("Invalid working directory '",base_dir,"'. Must end with 'StopTheSteal/Scripts.R'."))
@@ -22,20 +25,21 @@ str.input.file.path <- working_dir
 #setwd(working_dir)
 source("library_election_timeseries_analyzer.R")
 
+bool.rename.files <- FALSE # be careful with this indicator
+bool.convert.json.to.csv <- FALSE  # set it to TRUE to convert the existing (downloaded) JSON data set into CSV data set.
+bool.convert.csv.to.rds  <- FALSE
+bool.aggregate.csv.by.county <- FALSE
 str_election_date_YYYYMMDD <- "20210105"
-bool.convert.json.to.csv <- TRUE  # set it to TRUE to convert the existing (downloaded) JSON data set into CSV data set.
-negligible.choice.fraction.upper.threshold <- 0.05 # all election "choices" with votes fraction below this
-                                                   # threshold are merged into "votes_others" choice.
-bool.convert.csv.to.rds  <- TRUE
 
 # wget -O GA_S_1_5_2021`date +"%Y%m%d-%H%M%S"`.json https://static01.nyt.com/elections-assets/2020/data/api/2021-01-05/state-page/georgia.json
 # GASenateSpecialRunoff_2021_01_05_17_58_00_000.json
 # ...
 # GASenateSpecialRunoff_2021_01_06_20_21_19_000.json
 #######################################################################################################
-if(FALSE)
+if(bool.rename.files)
 {
   json_path <- paste0(str.input.file.path,"/input/elections-assets/2020/data/precincts")
+  json_path <- "C:/Users/JJohnson/Desktop/AllMyFiles/_Politics/GenElec2020/VoterRollsAnalysis/GA_Counts/NMG/loefflerk_warnockr"
   f <- as.data.frame(list.files(path = json_path, pattern = "nyt_GASenateSpecialRunoff-latest_.*json"), header=FALSE)
   colnames(f) <- 'files.old'
   f$files.new <- sapply(f$files.old,function(x) gsub("^[^.]*.",paste(gsub(".[^.]*$", "", x), '000.', sep='_'),x))
@@ -48,6 +52,7 @@ if(FALSE)
 
 if(bool.convert.json.to.csv)
 {
+  negligible.choice.fraction.upper.threshold <- 0.05 # all election "choices" with votes fraction below this threshold are merged into "votes_others" choice.
   arrStateAbbrs_Special <- c("GA")
   arrStateNames_Special <- c("Georgia") # GA
   list_file_prefixes_Special <- list(GA = "GASenateSpecialRunoff_")
@@ -139,6 +144,152 @@ if(bool.convert.csv.to.rds)
                           str_election_date_YYYYMMDD = str_election_date_YYYYMMDD)
   }
 } # END if(bool.convert.csv.to.rds)
+
+if(bool.aggregate.csv.by.county)
+{
+  str.input.path <- paste0(base_dir, "/input/elections-assets/2020/data/precincts/rds")
+  df_state20210105 <- readRDS(paste0(str.input.path,"/","state20210105.rds"))
+  df_GA20210105_county <- readRDS(paste0(str.input.path,"/","GA20210105_county.rds"))
+  df_GA20210105_county_vote_type <- readRDS(paste0(str.input.path,"/","GA20210105_county_vote_type.rds"))
+  df_GA20210105_precinct <- readRDS(paste0(str.input.path,"/","GA20210105_precinct.rds"))
+  df_GA20210105_precinct_vote_type <- readRDS(paste0(str.input.path,"/","GA20210105_precinct_vote_type.rds"))
+  df_GA20210105_precinct_vote_type_count_ts <- readRDS(paste0(str.input.path,"/","GA20210105_precinct_vote_type_count_ts.rds"))
+  df_GA20210105_vote_type <- readRDS(paste0(str.input.path,"/","GA20210105_vote_type.rds"))
+
+  df_GA20210105_precinct_vote_type_count_ts$tally <- as.integer(df_GA20210105_precinct_vote_type_count_ts$tally)
+  df_GA20210105_precinct_vote_type_count_ts$votes_warnockr <- as.integer(df_GA20210105_precinct_vote_type_count_ts$votes_warnockr)
+  df_GA20210105_precinct_vote_type_count_ts$votes_loefflerk <- as.integer(df_GA20210105_precinct_vote_type_count_ts$votes_loefflerk)
+  
+  df_GA20210105_precinct_vote_type_count_ts.by_sid <-
+    df_GA20210105_precinct_vote_type_count_ts %>%
+    group_by(time_offset_ms, sid) %>%
+    summarise(tally=sum(tally),
+              votes_warnockr=sum(votes_warnockr),
+              votes_loefflerk=sum(votes_loefflerk))
+  
+  df_GA20210105_precinct_vote_type_count_ts.by_sid_vid <-
+    df_GA20210105_precinct_vote_type_count_ts %>%
+    group_by(time_offset_ms, sid, vid) %>%
+    summarise(tally=sum(tally),
+              votes_warnockr=sum(votes_warnockr),
+              votes_loefflerk=sum(votes_loefflerk))
+  
+  df_GA20210105_precinct_vote_type_count_ts.by_sid_cid <-
+    df_GA20210105_precinct_vote_type_count_ts %>%
+    group_by(time_offset_ms, sid, cid) %>%
+    summarise(tally=sum(tally),
+              votes_warnockr=sum(votes_warnockr),
+              votes_loefflerk=sum(votes_loefflerk))
+  
+  df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid <-
+    df_GA20210105_precinct_vote_type_count_ts %>%
+    group_by(time_offset_ms, sid, cid, vid) %>%
+    summarise(tally=sum(tally),
+              votes_warnockr=sum(votes_warnockr),
+              votes_loefflerk=sum(votes_loefflerk))
+  
+  nrow(df_GA20210105_precinct_vote_type_count_ts) # 1,242,956
+  nrow(df_GA20210105_precinct_vote_type_count_ts.by_sid) # 117
+  nrow(df_GA20210105_precinct_vote_type_count_ts.by_sid_vid) # 468 = 4 * 117
+  nrow(df_GA20210105_precinct_vote_type_count_ts.by_sid_cid) # 18,602 = 4 * 116.99371069182389
+  nrow(df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid) # 74,408 = 4 * 117 * 158.991452991453
+  length(unique(df_GA20210105_precinct_vote_type_count_ts$time_offset_ms)) # 117
+  length(unique(df_GA20210105_county$sid)) # 1
+  length(unique(df_GA20210105_vote_type$vid)) # 4
+  length(unique(df_GA20210105_county$cid)) # 159
+
+  #######################################################################################################################
+  # Save/Plot the following time series:
+  #
+  # 1. Entire state (1)                  # df_GA20210105_precinct_vote_type_count_ts.by_sid
+  # 2. Entire state, each vote type (4). # df_GA20210105_precinct_vote_type_count_ts.by_sid_vid
+  # 3. Each county (159)                 # df_GA20210105_precinct_vote_type_count_ts.by_sid_cid
+  # 4. Each county, each vote type (636) # df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid
+  
+  # 1. Entire state (1)                  # df_GA20210105_precinct_vote_type_count_ts.by_sid
+  if(FALSE)
+  {
+    #head(df_GA20210105_precinct_vote_type_count_ts.by_sid)
+    df_curr_ts <- df_GA20210105_precinct_vote_type_count_ts.by_sid
+    df_curr_ts <- df_curr_ts[order(df_curr_ts$time_offset_ms),]
+    plot(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_warnockr / df_curr_ts$tally * 100)
+    lines(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_loefflerk / df_curr_ts$tally * 100)
+  }
+  
+  # 2. Entire state, each vote type (4). # df_GA20210105_precinct_vote_type_count_ts.by_sid_vid
+  if(FALSE)
+  {
+    #head(df_GA20210105_precinct_vote_type_count_ts.by_sid_vid)
+    arr_vids <- unique(df_GA20210105_precinct_vote_type_count_ts.by_sid_vid$vid)
+    arr_vids <- arr_vids[order(arr_vids)]
+    for(int_curr_vid in arr_vids)
+    {
+      df_curr_ts <- df_GA20210105_precinct_vote_type_count_ts.by_sid_vid[
+        df_GA20210105_precinct_vote_type_count_ts.by_sid_vid$vid == int_curr_vid,]
+      plot(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_warnockr / df_curr_ts$tally * 100)
+      lines(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_loefflerk / df_curr_ts$tally * 100)
+    }
+  }
+  # 3. Each county (159)                 # df_GA20210105_precinct_vote_type_count_ts.by_sid_cid
+  if(FALSE)
+  {
+    #head(df_GA20210105_precinct_vote_type_count_ts.by_sid_vid)
+    arr_cids <- unique(df_GA20210105_precinct_vote_type_count_ts.by_sid_cid$cid)
+    arr_cids <- arr_cids[order(arr_cids)]
+    for(int_curr_cid in arr_cids)
+    {
+      if(int_curr_cid <= 5)
+      {
+        df_curr_ts <- df_GA20210105_precinct_vote_type_count_ts.by_sid_cid[
+          df_GA20210105_precinct_vote_type_count_ts.by_sid_cid$cid == int_curr_cid,]
+        plot(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_warnockr / df_curr_ts$tally * 100)
+        lines(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_loefflerk / df_curr_ts$tally * 100)
+      }
+    }
+  }
+  # 4. Each county, each vote type (636) # df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid
+  if(FALSE)
+  {
+    #head(df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid)
+    arr_vids <- unique(df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid$vid)
+    arr_vids <- arr_vids[order(arr_vids)]
+    arr_cids <- unique(df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid$cid)
+    arr_cids <- arr_cids[order(arr_cids)]
+    for(int_curr_vid in arr_vids)
+    {
+      for(int_curr_cid in arr_cids)
+      {
+        if(int_curr_cid <= 5)
+        {
+          df_curr_ts <- df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid[
+            df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid$vid == int_curr_vid &
+              df_GA20210105_precinct_vote_type_count_ts.by_sid_cid_vid$cid == int_curr_cid,]
+          if(min(df_curr_ts$tally) < max(df_curr_ts$tally)) 
+          {
+            if(min(df_curr_ts$votes_warnockr) < max(df_curr_ts$votes_warnockr) &
+               min(df_curr_ts$votes_loefflerk) < max(df_curr_ts$votes_loefflerk))
+            {
+              plot(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_warnockr / df_curr_ts$tally * 100)
+              lines(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_loefflerk / df_curr_ts$tally * 100)
+            } else if(min(df_curr_ts$votes_warnockr) < max(df_curr_ts$votes_warnockr) &
+                      min(df_curr_ts$votes_loefflerk) == max(df_curr_ts$votes_loefflerk))
+            {
+              plot(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_warnockr / df_curr_ts$tally * 100)
+            } else if(min(df_curr_ts$votes_warnockr) == max(df_curr_ts$votes_warnockr) &
+                      min(df_curr_ts$votes_loefflerk) < max(df_curr_ts$votes_loefflerk))
+            {
+              plot(x = df_curr_ts$time_offset_ms, y = df_curr_ts$votes_loefflerk / df_curr_ts$tally * 100)
+            }
+          }
+        }
+      }
+    } # END for(int_curr_vid in arr_vids)
+  }
+  
+  
+  # TBD: convert to fractional format
+  # ...
+}
 
 ##########################################################################################################
 #
